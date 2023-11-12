e1158d7f807a0f769c4fa26cb9eb558950d0db23
Jung So Young <soyoung5611@gmail.com>
2023-11-13 03:33:44 +0900

    feat: Event Enum class
    - 이벤트 프로그램 열거

---
+++ src/main/java/christmas/domain/event/EventDay.java
@@ -0,0 +1,59 @@
+package christmas.domain.event;
+
+import christmas.domain.DayOfWeek;
+
+import java.util.List;
+
+public enum EventDay {
+
+    CHRISTMAS("크리스마스 디데이 할인", 1000),
+    WEEKDAY("평일 할인", 2023),
+    WEEKEND("주말 할인", 2023),
+    SPECIAL("특별 할인", 1000),
+    PRESENTATION("증정 이벤트", 25000),
+    SANTA_BADGE("산타", 20000),
+    TREE_BADGE("트리", 10000),
+    STAR_BADGE("별", 5000);
+
+
+    private String name;
+    private int eventPrice;
+
+    private static String NO_BADGE = "없음";
+
+    private static List<String> weekend = List.of(DayOfWeek.FRI.getDay(), DayOfWeek.SAT.getDay());
+
+    EventDay(String name, int eventPrice) {
+        this.name = name;
+        this.eventPrice = eventPrice;
+    }
+
+    public static String findBadge(int totalBenefitPrice) {
+        if(totalBenefitPrice >= SANTA_BADGE.getEventPrice()) {
+            return SANTA_BADGE.getName();
+        }
+        if(totalBenefitPrice >= TREE_BADGE.getEventPrice()) {
+            return TREE_BADGE.getName();
+        }
+        if(totalBenefitPrice >= STAR_BADGE.getEventPrice()) {
+            return STAR_BADGE.getName();
+        }
+        return NO_BADGE;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEventPrice() {
+        return eventPrice;
+    }
+    public static EventDay findEventDay(String weekDay) {
+        if(weekend.contains(weekDay)) {
+            return WEEKEND;
+        }
+        return WEEKDAY;
+    }
+
+}
