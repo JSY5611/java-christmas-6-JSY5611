0366682f3b3b5cc7b7cdc9a8f3c5b3dfc344b50e
Jung So Young <soyoung5611@gmail.com>
2023-11-13 03:40:56 +0900

    feat: 입력받은 날짜 확인 클래스
    - 입력받은 String을 int로 바꾼후 검증

---
+++ src/main/java/christmas/service/DayValidator.java
@@ -0,0 +1,28 @@
+package christmas.service;
+
+import christmas.domain.ErrorMessage;
+
+public class DayValidator {
+
+    private int day;
+
+    public DayValidator(String inputDay) {
+        this.day = Integer.parseInt(inputDay);
+        validateDay();
+    }
+
+    private void validateDay() {
+        if (!isValidDay(day)) {
+            throw new IllegalArgumentException(ErrorMessage.DAY_ERROR.getMessage());
+        }
+    }
+
+    private static boolean isValidDay(int day) {
+        return day >= 1 && day <= 31;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+}
