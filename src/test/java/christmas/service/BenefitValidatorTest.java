e271b268135f5d896fc6364e06a4363364b84ab6
Jung So Young <soyoung5611@gmail.com>
2023-11-15 23:00:27 +0900

    style: 주석 제거 및 변수 이름 변경
    - MENU3을 MENU2로 변경

--- src/test/java/christmas/service/BenefitValidatorTest.java
+++ src/test/java/christmas/service/BenefitValidatorTest.java
@@ -14,19 +14,15 @@ class BenefitValidatorTest {
     private EventValidator eventValidator;
     private BenefitValidator benefitValidator;
 
-    private static final String SUNDAY = "10"; //특별, 평일
-    private static final String FRIDAY = "29";// 주말
-    private static final String CHRISTMAS = "25"; // 특별, 평일
+    private static final String SUNDAY = "10";
 
     private static final String MENU = "양송이수프-1,티본스테이크-2,초코케이크-1,제로콜라-1";
-    private static final String MENU2 = "타파스-1,시저샐러드-1,바비큐립-1,해산물파스타-1,아이스크림-1,레드와인-1";
-
-    private static final String MENU3 = "양송이수프-1,제로콜라-1";
+    private static final String MENU2 = "양송이수프-1,제로콜라-1";
 
 
     @Test
     void 혜택_없음_테스트() {
-        menuValidator = new MenuValidator(MENU3);
+        menuValidator = new MenuValidator(MENU2);
         dayValidator = new DayValidator(SUNDAY);
         eventValidator = new EventValidator(dayValidator.getDay(), menuValidator.getTotalPrice());
         benefitValidator = new BenefitValidator(eventValidator, menuValidator);
