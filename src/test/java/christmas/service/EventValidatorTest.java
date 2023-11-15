185644ac83571f58a699416e44e789807d257ca7
Jung So Young <soyoung5611@gmail.com>
2023-11-15 23:01:20 +0900

    style: 주석 제거

--- src/test/java/christmas/service/EventValidatorTest.java
+++ src/test/java/christmas/service/EventValidatorTest.java
@@ -12,13 +12,9 @@ class EventValidatorTest {
     private MenuValidator menuValidator;
     private DayValidator dayValidator;
     private EventValidator eventValidator;
-
-    private static final String SUNDAY = "10"; //특별, 평일
-    private static final String FRIDAY = "29";// 주말
-    private static final String CHRISTMAS = "25"; // 특별, 평일
-
+    private static final String SUNDAY = "10";
+    private static final String FRIDAY = "29";
     private static final String MENU = "양송이수프-1,티본스테이크-1,초코케이크-1,제로콜라-1";
-    private static final String MENU2 = "타파스-1,시저샐러드-1,바비큐립-1,해산물파스타-1,아이스크림-1,레드와인-1";
 
     @Test
     void 평일_특별_크리스마스디데이_테스트() {
